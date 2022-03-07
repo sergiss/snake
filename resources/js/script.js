640090d9736310ddf35f671424bc629818ae8117
sergi <sergi.ss4@gmail.com>
2022-03-07 13:14:07 +0100

    Update Logic

--- resources/js/script.js
+++ resources/js/script.js
@@ -4,16 +4,16 @@ const CELL_SIZE = 16;
 const STEP_TIME = 1 / 60;
 
 const canvas = document.querySelector("#game");
-canvas.width  = WIDTH;
+canvas.width = WIDTH;
 canvas.height = HEIGHT;
 const ctx = canvas.getContext("2d");
 
 const snake = {};
-const board = {};
+var board = {};
 
 var startTime = 0, accum = 0;
 
-const step = (time)=> {
+const step = (time) => {
     requestAnimationFrame(step);
 
     const diff = time - startTime;
@@ -22,42 +22,42 @@ const step = (time)=> {
     accum += dt;
     while (accum >= STEP_TIME) {
         accum -= STEP_TIME;
-        
+
         // Update
         let coords = {};
         snake.time += 0.016 + 0.001 * snake.score;
-        if(snake.time > 1) {
+        if (snake.time > 1) {
             snake.time = 0;
             let lastIndex = snake.body[0];
             toCoords(lastIndex, coords);
-            let x = coords.x + snake.dirX;
-            let y = coords.y + snake.dirY;
+            let x = coords.x + (snake.dir > 1 ? 0 : (snake.dir === 1 ? 1 : -1));
+            let y = coords.y + (snake.dir < 2 ? 0 : (snake.dir === 2 ? 1 : -1));
             let index = toIndex(x, y);
-            if(snake.map[index] // body collision
-            || x < 0 || y < 0 || x >= WIDTH / CELL_SIZE || y >= HEIGHT / CELL_SIZE) { // bounds collision
+            if (snake.map[index] // body collision
+                || x < 0 || y < 0 || x >= WIDTH / CELL_SIZE || y >= HEIGHT / CELL_SIZE) { // bounds collision
                 // game over
                 init();
                 return;
             }
             snake.body[0] = index;
             snake.map = {};
-            for(let i = 1; i < snake.body.length; ++i) {
+            for (let i = 1; i < snake.body.length; ++i) {
                 index = snake.body[i];
                 snake.body[i] = lastIndex;
                 map[lastIndex] = true;
                 lastIndex = index;
             }
         }
-    
+
         // Render
         ctx.clearRect(0, 0, WIDTH, HEIGHT);
         ctx.fillStyle = "#0F0";
-        for(let i = 0; i < snake.body.length; ++i) {
+        for (let i = 0; i < snake.body.length; ++i) {
             toCoords(snake.body[i], coords);
             ctx.fillRect(
-                coords.x * CELL_SIZE, 
-                coords.y * CELL_SIZE, 
-                CELL_SIZE, 
+                coords.x * CELL_SIZE,
+                coords.y * CELL_SIZE,
+                CELL_SIZE,
                 CELL_SIZE
             );
         }
@@ -71,39 +71,42 @@ const toIndex = (x, y) => {
 }
 
 const toCoords = (index, result) => {
-    if(!result) result = {};
+    if (!result) result = {};
     result.x = index % WIDTH;
     result.y = (index / WIDTH) >> 0;
     return result;
 }
 
-const newGame = ()=> {
+const newGame = () => {
     snake.score = 0;
-    snake.body = [ toIndex(Math.floor(WIDTH / CELL_SIZE) >> 1, Math.floor(HEIGHT / CELL_SIZE) >> 1) ]
-    snake.dirX = 1; // Right
-    snake.dirY = 0;
+    snake.body = [toIndex(Math.floor(WIDTH / CELL_SIZE) >> 1, Math.floor(HEIGHT / CELL_SIZE) >> 1)]
+    snake.dir = 1; // Right
     snake.time = 0;
     snake.map = {};
+
+    board = {};
 }
 
-const init = ()=> {
-    document.addEventListener("keydown", (e)=> {
-        
-        switch(e.code) {
+const init = () => {
+
+    document.addEventListener("keydown", (e) => {
+        switch (e.code) {
             case "ArrowLeft":
-                snake.dirX = -1; snake.dirY = 0; snake.time = 2;
+                snake.dir = 0; snake.time = 2;
                 break;
             case "ArrowRight":
-                snake.dirX = 1; snake.dirY = 0; snake.time = 2;
-                break;
-            case "ArrowUp":
-                snake.dirX = 0; snake.dirY = -1; snake.time = 2;
+                snake.dir = 1; snake.time = 2;
                 break;
             case "ArrowDown":
-                snake.dirX = 0; snake.dirY = 1; snake.time = 2;
+                snake.dir = 2; snake.time = 2;
                 break;
+            case "ArrowUp":
+                snake.dir = 3; snake.time = 2;
+                break;
+
         }
     });
+
     newGame();
     step(0);
 }
