f919376d5b6e7d685447b4da876a881ebd5ac1d2
sergi <sergi.ss4@gmail.com>
2022-03-08 08:56:20 +0100

    Update

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,4 @@
-const WIDTH = 640, HEIGHT = 480;
+const WIDTH = 660, HEIGHT = 495;
 const CELL_SIZE = 32;
 const COLS = Math.floor(WIDTH  / CELL_SIZE);
 const ROWS = Math.floor(HEIGHT / CELL_SIZE);
@@ -10,114 +10,88 @@ canvas.width = WIDTH;
 canvas.height = HEIGHT;
 const ctx = canvas.getContext("2d");
 
-const snake = {};
+const snake = {time:0};
 var board = {};
 
 var startTime = 0, accum = 0;
 
 const step = (time) => {
+
     requestAnimationFrame(step);
     
-    let coords = {};
+    let index, coords = {};
     const diff = time - startTime;
     startTime = time;
-    const dt = diff / 1000;
-    accum += dt;
-    while (accum >= STEP_TIME) {
+    accum += diff / 1000;
+    while (accum >= STEP_TIME) { // fixed fps
         accum -= STEP_TIME;
-
         // Update
-        snake.time += 0.03 + 0.001 * snake.score;
-        if (snake.time >= 1) {
+        snake.time += 0.03 + 0.001 * snake.score; // dynamic difficulty
+        if (snake.time >= 1) { // update time
             snake.time = 0;
-            let index;
-            if(Math.random() < 0.075) {
+            if(Math.random() < 0.075) { // generate food
                 index = Math.floor(Math.random() * COLS * ROWS);
-                if(!snake.map[index]) board[index] = 1;
+                if(!board[index]) board[index] = 1;
             }
+            // Move head
             let lastIndex = snake.body[0];
             toCoords(lastIndex, coords);
-            let x = coords.x + (snake.dir > 1 ? 0 : (snake.dir === 1 ? 1 : -1));
-            let y = coords.y + (snake.dir < 2 ? 0 : (snake.dir === 2 ? 1 : -1));
+            let x = coords.x + (snake.dir & 0x3) - 1;
+            let y = coords.y + ((snake.dir >> 2) & 0x3) - 1;
             index = toIndex(x, y);
-            if (snake.map[index] // body collision
-            || x < 0 || y < 0 || x >= COLS || y >= ROWS) { // bounds collision
-                // game over
+            // Check collisions
+            if (board[index] === 2 // body
+            || x < 0 || y < 0 || x >= COLS || y >= ROWS) { // bounds
                 init();
-                // alert(`Game Over (Score: ${snake.score})`);
                 return;
-            }
-            if(board[index]) {
+            } else if(board[index]) { // food
                 snake.score += board[index];
                 snake.body.push(0);
-                board[index] = 2;
             }
+            // Move body
             delete board[snake.body[snake.body.length - 1]];
+            board[index] = 2;
             snake.body[0] = index;
-            snake.map = {};
             for (let i = 1; i < snake.body.length; ++i) {
                 index = snake.body[i];
                 snake.body[i] = lastIndex;
-                snake.map[lastIndex] = true;
                 lastIndex = index;
             }
- 
-        }
+        } // update time
 
-    }
+    } // while
 
     // Render
-    ctx.fillStyle = "#a5ce08";
-    ctx.fillRect(0, 0, WIDTH, HEIGHT);
-        
+    ctx.fillStyle = "#a5ce08"; // backgroun color
+    ctx.fillRect(0, 0, WIDTH, HEIGHT); // clear screen
+    // Draw body
     for (let i = 0; i < snake.body.length; ++i) {
         toCoords(snake.body[i], coords);
         ctx.fillStyle = i % 2 === 0 ? "#0F0" : "#9F9";
-        ctx.beginPath();
-        if(board[snake.body[i]] !== 2) {
-            ctx.fillRect(
-                coords.x * CELL_SIZE,
-                coords.y * CELL_SIZE,
-                CELL_SIZE,
-                CELL_SIZE
-            );
-        } else {
-            let hs = CELL_SIZE * 0.5;
-            ctx.arc(coords.x * CELL_SIZE + hs,
-                    coords.y * CELL_SIZE + hs,
-                    hs * 1.35, 0, Math.PI * 2);
-            ctx.fill();
-        }
+        ctx.fillRect(
+            coords.x + coords.x * CELL_SIZE,
+            coords.y + coords.y * CELL_SIZE,
+            CELL_SIZE,
+            CELL_SIZE
+        );
     }
-    
+    // Draw food
     Object.keys(board).map((cell)=> {
-        toCoords(cell, coords);
-        ctx.beginPath();
-        if(board[cell] === 1) {
+        if(board[cell] === 1) { // only food
+            toCoords(cell, coords);
             ctx.fillStyle = "#00F";
             ctx.fillRect(
-                coords.x * CELL_SIZE,
-                coords.y * CELL_SIZE,
+                coords.x + coords.x * CELL_SIZE,
+                coords.y + coords.y * CELL_SIZE,
                 CELL_SIZE,
                 CELL_SIZE
             );
-            ctx.stroke();
         }
     });
-    ctx.strokeStyle = "#87a81d";
-    ctx.beginPath();
-    for(let i = 1; i < COLS; ++i) {
-        ctx.moveTo(i * CELL_SIZE, 0);
-        ctx.lineTo(i * CELL_SIZE, CELL_SIZE * ROWS);
-       
-    }
-    ctx.stroke();
-    ctx.beginPath();
-    for(let i = 1; i < ROWS; ++i) {
-        ctx.moveTo(0, i * CELL_SIZE);
-        ctx.lineTo(CELL_SIZE * COLS, i * CELL_SIZE);
-    }
-    ctx.stroke();
+    // Score
+    ctx.font = "16px monospace"
+    ctx.fillStyle = "gray";
+    ctx.fillText(`Score: ${snake.score}`, 15, HEIGHT - 10);
 }
 
 const toIndex = (x, y) => {
@@ -132,48 +106,39 @@ const toCoords = (index, result) => {
 }
 
 const newGame = () => {
+    board = {}; // clear board
+    board[Math.floor(Math.random() * COLS * ROWS)] = 1;
     snake.score = 0;
-    let index = toIndex(COLS >> 1, ROWS >> 1);
-    snake.body = [];
-    for(let i = 0; i < 4; ++i) snake.body[i] = index;
-    snake.dir = 1; // Right
-    snake.time = 0;
-    snake.map = {};
-
-    board = {};
+    snake.body = []; // clear body
+    let index = toIndex(COLS >> 1, ROWS >> 1); // start position
+    for(let i = 0; i < 4; ++i) snake.body[i] = index; // body
+    snake.dir = 6; // Right
 }
 
 const init = () => {
-
     document.addEventListener("keydown", (e) => {
-        const lastDir = snake.dir;
+        let dir = snake.dir;
         switch (e.code) {
-            case "ArrowLeft":
-                if(snake.dir !== 1) {
-                    snake.dir = 0;
-                }
-                break;
             case "ArrowRight":
-                if(snake.dir !== 0) {
-                  snake.dir = 1;
-                }
+                  dir = 6;
+                break;
+            case "ArrowLeft":
+                  dir = 4;
                 break;
             case "ArrowDown":
-                if(snake.dir !== 3) {
-                  snake.dir = 2;
-                }
+                  dir = 9;
                 break;
             case "ArrowUp":
-                if(snake.dir !== 2) {
-                  snake.dir = 3;
-                }
+                  dir = 1;
                 break;
         }
-        if(lastDir !== snake.dir) snake.time = 1;
+        if(dir !== snake.dir) {
+            snake.dir = dir;
+            snake.time = 1;
+        }
     });
-
     newGame();
-    step(0);
+    step(0); // start game loop
 }
 
 init();
